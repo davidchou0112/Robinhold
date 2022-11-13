5313e520b7d63b2b465279f71782155ccdbe25ec
david <ypchou.david@gmail.com>
2022-11-13 12:46:18 -0800

    testing stocks reducer

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import configureStore from './store';
@@ -10,8 +11,10 @@ const store = configureStore();
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <BrowserRouter>
         <App />
-      </Provider>
-  </React.StrictMode>,
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode >,
   document.getElementById('root')
 );
---
+++ react-app/src/store/stocks.js
@@ -0,0 +1,53 @@
+// ========== CONSTANTS ==========
+
+const GET_ALL_STOCKS = 'stocks/displayAllStocks';
+
+
+// ========= REGULAR ACTION CREATOR ==========
+
+// all stocks
+const displayAllStocks = (stocks) => {
+    return {
+        type: GET_ALL_STOCKS,
+        stocks
+    }
+}
+
+// ========= THUNK ACTION CREATOR ==========
+
+// all stocks
+export const getAllStocks = () => async dispatch => {
+    const response = await fetch(`/stocks`);
+    console.log(response, 'this is response')
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(displayAllStocks(data));
+    }
+}
+
+// ========== STATE OBJECT ==========
+
+const initialState = { allStocks: {}, singleStock: {} };
+
+const stocksReducer = (state = initialState, action) => {
+    let newState = {};
+    switch (action.type) {
+
+        case GET_ALL_STOCKS:
+            let allStocks = {};
+            newState = {
+                ...state,
+                allStocks: { ...state.allStocks }
+            }
+            console.log(action, 'this is action')
+            action.stocks.Stock.forEach(stock => {
+                allStocks[stock.id] = stock;
+            });
+            newState.allStocks = allStocks
+            return newState
+
+        default:
+            return { ...state };
+    }
+}
+export default stocksReducer;
