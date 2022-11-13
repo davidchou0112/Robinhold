0688223e125cfa9b3913f6bd21d5c06785fe128a
david <ypchou.david@gmail.com>
2022-11-13 14:21:56 -0800

    individual stock page works

---
+++ react-app/src/components/Stock/singleStock.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getSingleStock } from '../../store/stocks';
+import Stock from './Stock'
+
+const SingleStock = () => {
+    const dispatch = useDispatch();
+    const { stockId } = useParams();
+    // console.log('this is stockId__:', stockId)
+
+    const singleStock = useSelector(state => state.stocks.singleStock)
+    // console.log('this is state__:', useSelector(state => state))
+    // console.log('this is singleStock__:', singleStock)
+
+    useEffect(() => {
+        dispatch(getSingleStock(stockId))
+    }, [])
+
+    return (
+        <div>
+            <h1>{singleStock.name}:{singleStock.symbol}</h1>
+            <h2>${singleStock.price}</h2>
+            <Stock />
+            <p>{singleStock.description}</p>
+        </div>
+
+    )
+}
+
+export default SingleStock
--- react-app/src/store/stocks.js
+++ react-app/src/store/stocks.js
@@ -1,10 +1,9 @@
 // ========== CONSTANTS ==========
-
 const GET_ALL_STOCKS = 'stocks/displayAllStocks';
+const GET_SINGLE_STOCK = 'stocks/displaySingleStock';
 
 
 // ========= REGULAR ACTION CREATOR ==========
-
 // all stocks
 const displayAllStocks = (stocks) => {
     return {
@@ -13,8 +12,16 @@ const displayAllStocks = (stocks) => {
     }
 }
 
-// ========= THUNK ACTION CREATOR ==========
+// single stocks
+const displaySingleStock = (singleStock) => {
+    return {
+        type: GET_SINGLE_STOCK,
+        singleStock
+    }
+}
 
+
+// ========= THUNK ACTION CREATOR ==========
 // all stocks
 export const getAllStocks = () => async dispatch => {
     const response = await fetch(`/stocks`);
@@ -28,14 +35,25 @@ export const getAllStocks = () => async dispatch => {
     }
 }
 
-// ========== STATE OBJECT ==========
+// single stock
+export const getSingleStock = (stockId) => async dispatch => {
+    const response = await fetch(`/stocks/${stockId}`);
+    if (response.ok) {
+        const data = await response.json();
+        console.log('this is data__:', data)
+        dispatch(displaySingleStock(data))
+    }
+}
 
+
+// ========== STATE OBJECT & REDUCER ==========
 const initialState = { allStocks: {}, singleStock: {} };
 
 const stocksReducer = (state = initialState, action) => {
     let newState = {};
     switch (action.type) {
 
+        // all stocks
         case GET_ALL_STOCKS:
             let allStocks = {};
             newState = {
@@ -47,6 +65,14 @@ const stocksReducer = (state = initialState, action) => {
                 allStocks[stock.id] = stock;
             });
             newState.allStocks = allStocks
+            return newState
+
+        // single stock
+        case GET_SINGLE_STOCK:
+            newState = {
+                ...state,
+                singleStock: { ...action.singleStock }
+            }
             return newState
 
         default:
