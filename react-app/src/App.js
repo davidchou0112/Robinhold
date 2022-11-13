157ccfa73b95ea3227ae3cee19a7aae10590968a
david <ypchou.david@gmail.com>
2022-11-12 22:23:42 -0800

    home page renders

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar';
+import NavBarNoLogin from './components/NavBarNoLogin';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
@@ -14,7 +15,7 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    (async() => {
+    (async () => {
       await dispatch(authenticate());
       setLoaded(true);
     })();
@@ -27,6 +28,7 @@ function App() {
   return (
     <BrowserRouter>
       <NavBar />
+      <NavBarNoLogin />
       <Switch>
         <Route path='/login' exact={true}>
           <LoginForm />
@@ -35,7 +37,7 @@ function App() {
           <SignUpForm />
         </Route>
         <ProtectedRoute path='/users' exact={true} >
-          <UsersList/>
+          <UsersList />
         </ProtectedRoute>
         <ProtectedRoute path='/users/:userId' exact={true} >
           <User />
