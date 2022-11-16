0a33f24d9d130f9b6300a84b976164453a050555
david <ypchou.david@gmail.com>
2022-11-16 08:05:40 -0800

    wed morning push to sync group code

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -75,79 +75,170 @@ const SignUpForm = () => {
   }
 
   return (
-    <div className='signup_body'>
-      <form className='justforwhite' onSubmit={onSignUp}>
-        <div className='error_message'>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
-        </div>
-        <div className='justforwhite'>
-          <label className='bblackfwhite'>User Name</label>
-          <input className='input_field'
-            type='text'
-            name='username'
-            onChange={updateUsername}
-            value={username}
-            placeholder='Between 4 and 12 characters'
-          ></input>
-        </div>
-        <div className='justforwhite'>
-          <label className='bblackfwhite'>First Name</label>
-          <input className='input_field'
-            type='text'
-            name='firstname'
-            onChange={updateFirstname}
-            value={firstname}
-            placeholder='Between 1 and 20 characters'
-          ></input>
-        </div>
-        <div className='justforwhite'>
-          <label className='bblackfwhite'>Last Name</label>
-          <input className='input_field'
-            type='text'
-            name='lastname'
-            onChange={updateLastname}
-            value={lastname}
-            placeholder='Between 1 and 20 characters'
-          ></input>
-        </div>
-        <div className='justforwhite'>
-          <label className='bblackfwhite'>Email</label>
-          <input className='input_field'
-            type='text'
-            name='email'
-            onChange={updateEmail}
-            value={email}
-            placeholder='Email'
-          ></input>
-        </div>
-        <div className='justforwhite'>
-          <label className='bblackfwhite'>Password</label>
-          <input className='input_field'
-            type='password'
-            name='password'
-            onChange={updatePassword}
-            value={password}
-            placeholder='Between 6 and 20 characters'
+    // <div className='signup_body'>
 
-          ></input>
+    //   <div id='signup_background'></div>
+    //   <div className='login_body'>
+    // <form className='justforwhite' onSubmit={onSignUp}>
+    //       <div className='error_message'>
+    //         {errors.map((error, ind) => (
+    //           <div key={ind}>{error}</div>
+    //         ))}
+    //       </div>
+    //       <div className='justforwhite'>
+    //         <label className='bblackfwhite'>User Name</label>
+    //         <input className='input_field'
+    //           type='text'
+    //           name='username'
+    //           onChange={updateUsername}
+    //           value={username}
+    //           placeholder='Between 4 and 12 characters'
+    //         ></input>
+    //       </div>
+    //       <div className='justforwhite'>
+    //         <label className='bblackfwhite'>First Name</label>
+    //         <input className='input_field'
+    //           type='text'
+    //           name='firstname'
+    //           onChange={updateFirstname}
+    //           value={firstname}
+    //           placeholder='Between 1 and 20 characters'
+    //         ></input>
+    //       </div>
+    //       <div className='justforwhite'>
+    //         <label className='bblackfwhite'>Last Name</label>
+    //         <input className='input_field'
+    //           type='text'
+    //           name='lastname'
+    //           onChange={updateLastname}
+    //           value={lastname}
+    //           placeholder='Between 1 and 20 characters'
+    //         ></input>
+    //       </div>
+    //       <div className='justforwhite'>
+    //         <label className='bblackfwhite'>Email</label>
+    //         <input className='input_field'
+    //           type='text'
+    //           name='email'
+    //           onChange={updateEmail}
+    //           value={email}
+    //           placeholder='Email'
+    //         ></input>
+    //       </div>
+    //       <div className='justforwhite'>
+    //         <label className='bblackfwhite'>Password</label>
+    //         <input className='input_field'
+    //           type='password'
+    //           name='password'
+    //           onChange={updatePassword}
+    //           value={password}
+    //           placeholder='Between 6 and 20 characters'
+
+    //         ></input>
+    //       </div>
+    //       <div className='justforwhite'>
+    //         <label className='bblackfwhite'>Repeat Password</label>
+    //         <input className='input_field'
+    //           type='password'
+    //           name='repeat_password'
+    //           onChange={updateRepeatPassword}
+    //           value={repeatPassword}
+    //           required={true}
+    //           placeholder='Repeat Password'
+    //         ></input>
+    //       </div>
+    //       <button className='body__button' id='body__button_3' type='submit'>Sign Up</button>
+    //       <div className='no_account'><small className='bblackfwhite'>Already started? <NavLink className='blackblue' to='/login' exact={true}>Log in to complete your application</NavLink></small></div>
+    //     </form>
+    //   </div>
+    // </div>
+
+    <div className='justforwhite1'>
+      <div className='login_wrapper'>
+        <div id='signup_background'></div>
+        <div className='login_body'>
+          <form className='login_context' onSubmit={onSignUp}>
+            <div className='login_header'><strong className='bblackfwhite' id='auth_label'>Sign up to Robinhold</strong></div>
+
+            <div className='login_form'>
+              <div className='error_message'>
+                {errors.map((error, ind) => (
+                  <div className='error_message' key={ind}>{error}</div>
+                ))}
+              </div>
+              <label className='bblackfwhite' htmlFor='email'>User Name</label>
+              <div className='bblackfwhite'>
+                <input className='input_field'
+                  type='text'
+                  name='username'
+                  onChange={updateUsername}
+                  value={username}
+                  placeholder='Between 4 and 12 characters'
+                />
+              </div>
+              <label className='bblackfwhite' htmlFor='email'>First Name</label>
+              <div className='bblackfwhite'>
+                <input className='input_field'
+                  type='text'
+                  name='firstname'
+                  onChange={updateFirstname}
+                  value={firstname}
+                  placeholder='Between 1 and 20 characters'
+                />
+              </div>
+              <label className='bblackfwhite' htmlFor='email'>Last Name</label>
+              <div className='bblackfwhite'>
+                <input className='input_field'
+                  type='text'
+                  name='lastname'
+                  onChange={updateLastname}
+                  value={lastname}
+                  placeholder='Between 1 and 20 characters'
+                />
+              </div>
+              <label className='bblackfwhite' htmlFor='email'>Email</label>
+              <div className='bblackfwhite'>
+                <input className='input_field'
+                  type='text'
+                  name='email'
+                  onChange={updateEmail}
+                  value={email}
+                  placeholder='Email'
+                />
+              </div>
+              <label className='bblackfwhite' htmlFor='email'>Password</label>
+              <div className='bblackfwhite'>
+                <input className='input_field'
+                  type='password'
+                  name='password'
+                  onChange={updatePassword}
+                  value={password}
+                  placeholder='Between 6 and 20 characters'
+                />
+              </div>
+              <label className='bblackfwhite' htmlFor='password'>Repeat Password</label>
+              <div className='bblackfwhite'>
+                <input className='input_field'
+                  type='password'
+                  name='repeat_password'
+                  onChange={updateRepeatPassword}
+                  value={repeatPassword}
+                  required={true}
+                  placeholder='Repeat Password'
+                />
+              </div>
+
+              <div className='button_field'>
+                <button className='body__button' id='body__button_3' type='submit'>Sign Up</button>
+              </div>
+              <div className='no_account'><small className='bblackfwhite'>Already started? <NavLink className='blackblue' to='/login' exact={true}>Log in to complete your application</NavLink></small></div>
+            </div>
+          </form>
         </div>
-        <div className='justforwhite'>
-          <label className='bblackfwhite'>Repeat Password</label>
-          <input className='input_field'
-            type='password'
-            name='repeat_password'
-            onChange={updateRepeatPassword}
-            value={repeatPassword}
-            required={true}
-            placeholder='Repeat Password'
-          ></input>
-        </div>
-        <button className='body__button' id='body__button_3' type='submit'>Sign Up</button>
-        <div className='no_account'><small className='bblackfwhite'>Already started? <NavLink className='blackblue' to='/login' exact={true}>Log in to complete your application</NavLink></small></div>
-      </form>
+      </div >
     </div>
+
+
   );
 };
 
