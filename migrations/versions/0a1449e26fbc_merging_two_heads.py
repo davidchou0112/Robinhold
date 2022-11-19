8eaf84e5284cb0f37eac3858ce037cb8dbd6a958
david <ypchou.david@gmail.com>
2022-11-19 02:11:55 -0800

    test

---
+++ migrations/versions/0a1449e26fbc_merging_two_heads.py
@@ -0,0 +1,24 @@
+"""merging two heads
+
+Revision ID: 0a1449e26fbc
+Revises: 946762a68cc1, 22a5320d972d
+Create Date: 2022-11-18 19:22:48.561954
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0a1449e26fbc'
+down_revision = ('946762a68cc1', '22a5320d972d')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
